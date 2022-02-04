b454ce248cec0c1006fee84244e747b6261b962a
suresmaharjan <suresmaharjan@gmail.com>
2022-02-04 16:43:12 +0545

    spinner added

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,10 +1,48 @@
+import { useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
 import styled from "styled-components";
 
-export const Featured=styled.section`
+const HeroImg = styled.div`
     width:100vw;
     height:100vh;
-    background-image: url('https://source.unsplash.com/random');
-    background-size:cover;
-    background-position:center;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    position:relative;
+
+    img{
+        width:100vw;
+        height:100vh;
+        object-fit:cover;
+    }
+    
+    .spinner{
+        position:absolute;
+        top:0;
+        bottom:0;
+        margin:auto;
+    }
+
 `
+
+
+export const Featured = () => {
+    const [loading, setLoading] = useState(true);
+    useEffect(()=>{
+        fetch("https://source.unsplash.com/random")
+        .then(res => res.status === 200  && setLoading(false))
+    })
+
+
+    return (
+        <HeroImg>
+            <img src="https://source.unsplash.com/random" />
+           { loading && ( 
+            <Spinner animation="border" role="status" className="spinner">
+            </Spinner> )}
+        </HeroImg>
+    )
+}
+
+
 
